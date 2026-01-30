8d84ed09c2b3ce4ac4b40c196cdc93bed200d92d
lynsunwoo <lynsunwoo@gmail.com>
2026-01-30 10:19:29 +0900

    Add files via upload

---
+++ Join.js
@@ -0,0 +1,188 @@
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import axios from 'axios';
+
+function Join(props) {
+  //변수 선언
+  const [form, setForm] = useState({
+    username: '',
+    password: '',
+    password2: '',
+    email: '',
+    tel: ''
+  });
+
+  //회원가입 실패 변수
+  const [error, setError] = useState('');
+  const [success, setSuccess] = useState('');
+
+  //페이지 이동
+  const navigate = useNavigate();
+
+  //아이디 중복체크
+  const checkUsername = () => {
+    // if(!form.username){
+    //   alert('아이디를 입력하세요');
+    //   return;
+    // }
+
+    // axios.get(`http://localhost:9070/check-username/${form.username}`)
+    // .then(res => {
+    //   setSuccess('사용 가능한 아이디입니다.');
+    // })
+    // .catch(()=>{
+    //   setError('이미 사용중인 아이디입니다');
+    // });
+
+    // axios.get(`http://localhost:9070/check-username/${form.username}`)
+    axios.post('https://port-0-ginipet-backend-mkvwe6z7891e08f1.sel3.cloudtype.app/check-username', {
+      username: form.username
+    })
+      .then(res => {
+        if (res.data.exists) {
+          setError('이미 사용중인 아이디입니다.');
+        } else {
+          setSuccess('사용 가능한 아이디입니다.');
+        }
+      });
+  };
+
+  //회원가입 성공 변수
+  const [agree, setAgree] = useState(false);
+
+  //입력한 값 저장하기
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setForm(prev => ({
+      ...prev, [name]: value
+    }))
+
+    //데이터가 없으면 에러를 띄우기
+    setError(''); //에러 초기화
+    setSuccess(''); //성공 초기화
+  }
+
+  //회원가입 버튼 클릭시 해당 값들을 서버측으로 전송함
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    //비밀번호, 비밀번호 확인 
+    if (form.password !== form.password2) {
+      setError('비밀번호가 일치하지 않습니다');
+      return;
+    }
+
+    //약관 동의 체크
+    if (!agree) {
+      alert('개인 정보처리 방침에 동의해주세요');
+      return;
+    }
+
+    // axios.get(`http://localhost:9070/check-username/${form.username}`)
+    axios.post('https://port-0-ginipet-backend-mkvwe6z7891e08f1.sel3.cloudtype.app/register', form)
+      .then(res => {
+        alert('회원가입이 완료되었습니다. 로그인 페이지로 이동합니다.');
+        navigate('/login');
+        console.log(res.data);
+      })
+      .catch(err => {
+        console.error(err);
+        setError('회원가입 실패 : 아이디가 이미 존재하거나 서버 오류입니다.');
+      });
+  };
+
+  return (
+    <main>
+      <section className='join_box'>
+        <h2>회원가입</h2>
+        <form onSubmit={handleSubmit} className="join_form">
+          <p className='join_p'>
+            <label htmlFor="username">아이디 :</label>
+            <input
+              type="text"
+              id='username'
+              name='username'
+              placeholder='아이디 입력'
+              value={form.username}
+              onChange={handleChange}
+              required
+            />
+            <button type='button' onClick={checkUsername}>중복확인</button>
+          </p>
+
+          <p className='join_p'>
+            <label htmlFor="password">패스워드 :</label>
+            <input
+              type="password"
+              id='password'
+              name='password'
+              placeholder='패스워드 입력'
+              value={form.password}
+              onChange={handleChange}
+              required
+            />
+          </p>
+
+          <p className='join_p'>
+            <label htmlFor="password2">패스워드 확인 :</label>
+            <input
+              type="password"
+              id='password2'
+              name='password2'
+              placeholder='패스워드 입력'
+              value={form.password2}
+              onChange={handleChange}
+              required
+            />
+          </p>
+
+          <p className='join_p'>
+            <label htmlFor="email">이메일 주소 :</label>
+            <input
+              type="email"
+              id='email'
+              name='email'
+              placeholder='id@domain.co.kr'
+              value={form.email}
+              onChange={handleChange}
+              required
+            />
+          </p>
+
+          <p className='join_p'>
+            <label htmlFor="tel">전화번호 :</label>
+            <input
+              type="tel"
+              id='tel'
+              name='tel'
+              placeholder='000-0000-0000'
+              value={form.tel}
+              onChange={handleChange}
+              required
+            />
+          </p>
+
+          <p className='check_save'>
+            <input
+              type="checkbox"
+              required
+              id="agree"
+              checked={agree}
+              onChange={(e) => setAgree(e.target.checked)}
+            />
+            <label htmlFor="agree">이용약관, 개인정보 수집 및 이용, 마케팅 활용 선택에 모두 동의합니다.</label>
+          </p>
+          <p className='submit_btn'>
+            <button type='submit'>회원가입</button>
+          </p>
+
+          {/* 회원가입이 실패하면 나오는 문구 */}
+          {error && <p style={{ color: '#f00' }}>{error}</p>}
+          {success && <p style={{ color: '#0f0' }}>{success}</p>}
+        </form>
+      </section>
+    </main>
+  );
+}
+
+export default Join;
---
+++ Login.js
@@ -0,0 +1,149 @@
+import React, { useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import axios from 'axios';
+
+function Login(props) {
+  //상태 변수 선언
+  const [form, setForm] = useState({
+    username: '',
+    password: '',
+  });
+
+  const [error, setError] = useState('');
+
+  const navigate = useNavigate();
+
+  //회원 비회원 폼 변경 상태 변수
+  const [memberType, setMemberType] = useState('member');
+
+  //입력폼에 입력시 실행되는 함수
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setForm(prev => ({
+      ...prev, [name]: value
+    }))
+    setError(''); //에러 초기화
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // axios.post('http://localhost:9070/login', form)
+    axios.post('https://port-0-ginipet-backend-mkvwe6z7891e08f1.sel3.cloudtype.app/login', form)
+      .then(res => {
+        //JWT 토근 저장
+        localStorage.setItem('token', res.data.token);
+        localStorage.setItem('username', form.username); //사용자 아이디 저장
+
+        alert('로그인이 완료 되었습니다. 메인페이지로 이동합니다.');
+        navigate('/');
+      })
+      .catch(err => {
+        console.error(err);
+        setError('로그인 실패: 아이디와 패스워드가 존재하지 않습니다.')
+      });
+  };
+
+  //휴대폰 본인 인증 ui 
+  const handlePhoneAuth = () =>{
+    alert('휴대폰 본인 인증')
+  }
+  return (
+    <main>
+      <section className='login_box'>
+        <h2>로그인</h2>
+        <form onSubmit={handleSubmit} className="login_form">
+          <p className='form_p'>
+
+            <label htmlFor="member">
+              <input
+                type="radio"
+                id='member'
+                name='memberGroup'
+                value='member'
+                checked={memberType === 'member'}
+                onChange={(e) => setMemberType(e.target.value)}
+              />
+              회원
+            </label>
+
+            <label htmlFor="nomember">
+              <input
+                type="radio"
+                id='nomember'
+                name='memberGroup'
+                value='nomember'
+                checked={memberType === 'nomember'}
+                onChange={(e) => setMemberType(e.target.value)}
+              />
+              비회원
+            </label>
+          </p>
+          {memberType === 'member' && (
+            <>
+              <p className='form_p'>
+                <label htmlFor="username">아이디</label>
+                <input
+                  type="text"
+                  id='username'
+                  name='username'
+                  placeholder='아이디'
+                  required
+                  onChange={handleChange}
+                  value={form.username}
+                />
+              </p>
+
+              <p className='form_p'>
+                <label htmlFor="password">비밀번호</label>
+                <input
+                  type="password"
+                  id='password'
+                  name='password'
+                  placeholder='패스워드'
+                  required
+                  onChange={handleChange}
+                  value={form.password}
+                />
+              </p>
+
+              <p className='id_save'>
+                <label htmlFor="username_check">
+                  <input
+                    type="checkbox"
+                    id='username_check'
+                  />아이디 저장
+                </label>
+              </p>
+
+              <p className='submit_btn'>
+                {/* <input type="submit" value="로그인" /> 
+            button 태그 - 확장 가능한 버튼, 문자, 이미지, 아이콘 , html 요소 모두 사용이 가능하여 자유롭게 css로 서식이 변경가능함. 스타일 적용이 쉬워 리액트에서 주로 많이 사용함.
+            input typw="submit" - 내용을 단순하게 전송하기 위한 목적, 버튼안에 텍스트만 들어가며 아이콘, 이미지, 로딩바 적용이 쉽지 않음.
+            */}
+                <button type='submit'>로그인</button>
+              </p>
+
+              <p className='link_group'>
+                <Link to="/id_search">아이디 찾기</Link> &#10072;
+                <Link to="/pw_search">비밀번호 찾기</Link> &#10072;
+                <Link to="/join">회원가입</Link>
+              </p>
+
+              {error && <p style={{ color: '#f00' }}>{error}</p>}
+            </>
+          )}
+          {memberType === 'nomember' && (
+            <>
+              <p className='auth_p'>본인 인증수단을 통해 인증 후 비회원 주문 및 구매내역을 확인하실 수 있습니다.</p>
+              <p className='phone_auth'>
+                <button type='submit' onClick={handlePhoneAuth}>휴대폰 본인 인증</button>
+              </p>
+            </>
+          )}
+        </form>
+      </section>
+    </main>
+  );
+}
+
+export default Login;
